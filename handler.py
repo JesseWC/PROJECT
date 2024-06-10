58fef7296695ae6a4bf0472ce089757a908769d0
JesseWC <148896523+JesseWC@users.noreply.github.com>
2024-06-10 13:20:10 -0400

    June 10th

---
+++ handler.py
@@ -0,0 +1,62 @@
+import pygame
+import time
+
+from Ball import Ball
+from Goal import Goal
+from Avatar import Avatar
+
+
+
+score=0
+# set up pygame modules
+pygame.init()
+pygame.font.init()
+font2=pygame.font.SysFont('calibri', 35)
+font = pygame.font.SysFont('comicsansms', 50)
+displayintro=font.render("Click Anywhere To Continue", True, (255, 255, 255))
+displayscore=font2.render("Score: "+ str(score), True, (255, 255, 255))
+intro_over=False
+# set up variables for the display
+size = (800, 500)
+screen = pygame.display.set_mode(size)
+
+
+
+
+a=Goal(40,40)
+
+
+
+
+
+
+
+
+
+
+running = True
+while running:
+    screen.fill((255, 0, 255))
+    screen.blit(displayintro, (75, 200))
+
+
+
+
+
+
+
+
+   
+    for event in pygame.event.get():  
+        pygame.display.update()
+        if event.type == pygame.QUIT:  
+            running = False
+        if event.type == pygame.KEYDOWN:
+            intro_over=True
+        if intro_over==True:
+            screen.blit(displayscore, (0, 0))
+
+
+screen.blit(a.image, a.rect)
+#screen.blit(b.image, b.rect)
+#screen.blit(c.image, c.rect)
